8019dec642ae20ab0837577d442f07b1d13ac59b
luiscarrion75 <lxcarrion1@utpl.edu.ec>
2022-10-23 15:17:42 -0500

    Git

---
+++ src/Git.java
@@ -0,0 +1,13 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("COMANDOS GIT");
+        System.out.println("-git branch: Se usa el comando git branch para crear, enumerar y eliminar ramas");
+        System.out.println("-git status: Nos brinda toda la información necesaria sobre la rama actual.");
+        System.out.println("-git add: Se usa el comando git add para incluir los cambios de un archivo en nuestro próximo commit.");
+        System.out.println("-git commit: Se usa para establecer un punto de control en el proceso de desarrollo al que puede volver más tarde si es necesario.");
+        System.out.println("-git push: Este comando sube las confirmaciones al repositorio remoto.");
+        System.out.println("-git init: Creará un nuevo repositorio local GIT");
+        System.out.println("-git config: Se usa para establecer una configuración específica de usuario, como el email, nombre de usuario y tipo de formato");
+        System.out.println("-git remote: Permite ver todos los repositorios remotos.");
+    }
+}
