7cc4560a8885bc5ca4d8e146f17e64759a18ed46
luiscarrion75 <lxcarrion1@utpl.edu.ec>
2022-10-23 14:49:45 -0500

    Clase programacion

---
+++ src/contenido.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+
+public class contenido {
+    public static void main(String[] args) {
+        System.out.println("CONCEPTOS BASICOS DE LA PROGRAMACION");
+        System.out.println("-Algoritmo");
+        System.out.println("-Lenguaje");
+        System.out.println("-Codigo");
+        System.out.println("-Codigo fuente");
+        System.out.println("-Codigo maquina");
+        System.out.println("-Codigo binario");
+        System.out.println("-Bug");
+        System.out.println("-Refacterizacion");
+        System.out.println("-Frameworks");
+        System.out.println("-Queries");
+        System.out.println("-Git");
+        System.out.println("-Backdoor");
+        System.out.println("-Backup");
+        System.out.println("-Big data");
+        System.out.println("-Bit");
+        System.out.println("-Boot");
+        System.out.println("-Byte");
+        System.out.println("-Console");
+        System.out.println("-Sistema operativo");
+        System.out.println("-Plugin");
+    }
+}
