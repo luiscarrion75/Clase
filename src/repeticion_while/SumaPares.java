80eaaf74cddbcd3c7d95691858e11f3e6ab2480a
luiscarrion75 <lxcarrion1@utpl.edu.ec>
2022-11-16 11:56:38 -0500

    CondicionalTarea

--- src/repeticion_while/SumaPares.java
+++ src/repeticion_while/SumaPares.java
@@ -7,12 +7,12 @@ public class SumaPares {
     public static void main(String[] args) {
         System.out.println("SUMA DE NUMERO PARES DEL 1 AL 100");
         int suma_pares = 0;
-        int num = 2;
-        while (num<=100){
+        int num = 100;
+        while (num>=2){
             if(num%2==0){
                 suma_pares = suma_pares + num;
             }
-            num = num +1;
+            num--;//num = num +1;
         }
         System.out.println("La suma de los numeros pares es: "+ suma_pares);
     }
