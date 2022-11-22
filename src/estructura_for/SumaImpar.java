6609d00f9347af3f2dac4e4dda8d9e0f3d23b11e
luiscarrion75 <lxcarrion1@utpl.edu.ec>
2022-11-22 17:01:35 -0500

    Estructuras_For

---
+++ src/estructura_for/SumaImpar.java
@@ -0,0 +1,16 @@
+package estructura_for;
+
+import java.util.Scanner;
+
+public class SumaImpar {
+    public static void main(String[] args) {
+        System.out.println("Ingrese un numero limite: ");
+        Scanner teclado = new Scanner(System.in);
+        int limite = teclado.nextInt();
+        int suma_impares = 0;
+        for (int contador = 1; contador <= limite; contador = contador+2){
+            suma_impares = suma_impares + contador;
+        }
+        System.out.println("La suma de numeros impares = "+suma_impares);
+    }
+}
